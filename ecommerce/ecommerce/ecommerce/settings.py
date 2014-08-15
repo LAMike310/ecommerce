09543a23e8033d18257b5c5cab635338d6e8c775
Mike Gnanakone <mvg210@gmail.com>
2014-08-15 14:58:41 -0700

    Added a products app and created a homepage

--- ecommerce/ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/ecommerce/settings.py
@@ -1,4 +1,4 @@
- """
+"""
 Django settings for ecommerce project.
 
 For more information on this file, see
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 )
 
 MIDDLEWARE_CLASSES = (
