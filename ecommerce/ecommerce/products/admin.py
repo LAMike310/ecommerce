278542a86356ee549ad2451ce059df2d76f27e04
Mike Gnanakone <mvg210@gmail.com>
2014-08-18 13:47:32 -0700

    Customized admin to filter products  by date and product name

--- ecommerce/ecommerce/products/admin.py
+++ ecommerce/ecommerce/products/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	date_hierarchy = 'time'
+	search_fields = ['title', 'description']
+	list_display = ['title', 'price', 'active', 'updated']
+	list_editable = ['price', 'active']
+	list_filter = ['price', 'active']
+	readonly_fields = ['time', 'updated']
+	prepopulated_fields = {"slug": ("title",)}
+	class Meta:
+		model = Product
+
+admin.site.register(Product, ProductAdmin)
+
