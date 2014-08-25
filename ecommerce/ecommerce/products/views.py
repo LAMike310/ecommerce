3bfd9a4b676f754a1c91da7ddbdfe3a3993e61b3
Mike Gnanakone <mvg210@gmail.com>
2014-08-25 15:44:31 -0700

    Created unique pages for each product

--- ecommerce/ecommerce/products/views.py
+++ ecommerce/ecommerce/products/views.py
@@ -16,3 +16,22 @@ def all(request):
 	context = {'products': products }
 	template = 'products/all.html'
 	return render(request, template, context)
+
+# def single(request, slug):
+# 	try:
+# 		product = Product.objects.get(slug=slug)
+# 		print product.title
+# 		context = {'product': product }
+# 		template = 'products/single.html'
+# 		return render(request, template, context)
+# 	except:
+# 		raise Http404
+
+def single(request, slug):
+	try:
+		product = Product.objects.get(slug=slug)
+		context = {'product': product}
+		template = 'products/single.html'	
+		return render(request, template, context)
+	except:
+		raise Http404
