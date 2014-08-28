523fc801de1f0c4740cec9b40427d93d49ef9743
Mike Gnanakone <mvg210@gmail.com>
2014-08-28 13:50:50 -0700

    Added carts app

--- ecommerce/ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/ecommerce/urls.py
@@ -7,17 +7,17 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'products.views.home', name='home'),
-    url(r's/$', 'products.views.search', name='search'),
+    url(r'^s/$', 'products.views.search', name='search'),
     url(r'^products/$', 'products.views.all', name='products'),
     url(r'^products/(?P<slug>[\w-]+)/$', 'products.views.single', name='single_product'),
-
+    url(r'^cart/$', 'carts.views.view', name='cart'),
     # url(r'^blog/', include('blog.urls')),
-    # (?P<all_items).*)
-	# (?P<id>\d+)
+    #(?P<all_items>.*)
+    #(?P<id>\d+)
     url(r'^admin/', include(admin.site.urls)),
 ) 
 
 
 if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
