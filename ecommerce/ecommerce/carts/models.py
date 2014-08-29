1fdc6b8e5377fe1a41438b3927401bd0edb56bb2
Mike Gnanakone <mvg210@gmail.com>
2014-08-29 16:11:50 -0700

    refactored cart app

--- ecommerce/ecommerce/carts/models.py
+++ ecommerce/ecommerce/carts/models.py
@@ -4,19 +4,20 @@ from django.db import models
 from products.models import Product
 
 class CartItem(models.Model):
+	cart = models.ForeignKey('Cart', null=True, blank=True)
 	product = models.ForeignKey(Product, null=True, blank=True)
 	quantity = models.IntegerField(default=1)
 	time = models.DateTimeField(auto_now_add=True, auto_now=False)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
 
 	def __unicode__(self):
-		return self.product.title
-
+		try:
+			return str(self.cart.id)
+		except:
+			return self.product.title
 
 
 class Cart(models.Model):
-	items = models.ManyToManyField(CartItem, null=True, blank=True)
-	products = models.ManyToManyField(Product, null=True, blank=True)
 	total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
 	time = models.DateTimeField(auto_now_add=True, auto_now=False)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
