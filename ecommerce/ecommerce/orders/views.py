2f5466c6dcfbb62bd5bd74a7d48b44df366792e2
Mike Gnanakone <mvg210@gmail.com>
2014-09-15 17:22:14 -0700

    Added a checkout button

--- ecommerce/ecommerce/orders/views.py
+++ ecommerce/ecommerce/orders/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import time
+
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
+
+from carts.models import Cart
+from .models import Order
+
+def checkout(request):
+	try:
+		the_id = request.session['cart_id']
+		cart = Cart.objects.get(id=the_id)
+		print cart
+	except:
+		the_id = None
+		# return HttpResponseRedirect(reverse("cart"))
+		return HttpResponseRedirect(reverse("cart"))
+
+	new_order, created = Order.objects.get_or_create(cart=cart)
+	if created:
+		# assign a user 
+		# subtotal 
+		# tax
+		# final price
+		new_order.order_id = str(time.time())
+		new_order.save()
+	# assign price	
+	# run credit card info
+	if new_order.status == "Finished":
+		del request.session['cart_id']
+		del request.session['items_total']
+		return HttpResponseRedirect(reverse("cart"))
+
+	context = {}
+	template = "products/home.html"
+	return render(request, template, context)
